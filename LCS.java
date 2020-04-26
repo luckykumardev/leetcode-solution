d721d2ef6cdcfad0d8a992bc29b9ab770a30ed04
Your Name <you@example.com>
2020-04-26 01:38:41 -0700

    LCS DP

--- LCS.java
+++ LCS.java
@@ -2,48 +2,15 @@ class Solution {
 
     public int longestCommonSubsequence(String s, String t) {
     
-         
-        int memo[][] = new int[s.length() +1][t.length() +1];
-        int ans = longestCommonSubsequence(s, t, memo);
+         //memoization
+        // int memo[][] = new int[s.length() +1][t.length() +1];
+        // int ans = longestCommonSubsequence(s, t, memo);
+        
+           int ans = longestCommonSubsequenceDP(s, t);
+        
         return ans;
          
     }
- 
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
      public int longestCommonSubsequence(String s, String t, int memo[][]) {
          if(s.length() == 0  || t.length() == 0)
@@ -65,11 +32,26 @@ class Solution {
         }
 
     }
+    public int longestCommonSubsequenceDP(String s, String t)   {
+        int m = s.length();
+        int n = t.length();
+        
+        int dp[][] = new int[m+1][n+1];
+         
+        for(int i =1; i <=m; i++){
+            for(int j =1; j <=n; j++){
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        return dp[m][n];
+    }
     
     
 }
-
-
 
 
 
