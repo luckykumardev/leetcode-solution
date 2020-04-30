76830b7ae8cea3f6e60e2aef190dcd3273f20988
Your Name <you@example.com>
2020-04-30 01:39:42 -0700

    leetcode day 30

---
+++ Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValidSequence(TreeNode root, int[] arr) {
+
+        return existPath(root, arr, 0);
+        
+    }
+    
+    
+    public static boolean existPath(TreeNode root, int arr[], int index){
+         if(root==null || index >= arr.length)  
+            return arr.length==0; 
+        
+  
+        if((root.left==null && root.right==null) && (root.val==arr[index]&& root.val==arr[arr.length-1])) 
+            return true; 
+       
+        boolean ans = false;
+        if(root.val==arr[index]){
+            ans = existPath(root.left,arr,index+1) || existPath(root.right, arr, index+1);
+            
+        }
+        
+        return ans;
+    }
+}
+
--- Unique Binary Search Trees.java
+++ Unique Binary Search Trees.java
@@ -1,10 +1,15 @@
-class Solution {
-   public int numTrees(int n) {      
+
+class Solution {  
+  
+  ////////////////
+ ///recursion ///
+////////////////
+  public int numTrees(int n) {      
         return numTrees(1, n);
     }
-
+    
     private int numTrees(int start, int end) {
-
+        
         if (start >= end) {
             return 1;
         }        
@@ -14,4 +19,50 @@ class Solution {
         }        
         return countUnique;
     }
+
+
+
+
+  ////////////////
+ ///memoization//
+////////////////
+public int numTrees(int n) {
+        
+        int[][] memo = new int[n + 1][n + 1];
+        return numTrees(1, n, memo);
+    }
+    
+    private int numTrees(int start, int end, int[][] memo) {
+        
+        if (start >= end) {
+            return 1;
+        }        
+        if (memo[start][end] > 0) {
+            return memo[start][end];
+        }        
+        int countUnique = 0;        
+        for (int i = start; i <= end; i++) { // take i as root
+            countUnique += numTrees(start, i - 1, memo) * numTrees(i + 1, end, memo);
+        }        
+        memo[start][end] = countUnique;
+        
+        return countUnique;
+    }
+
+
+
+
+  ////////////////
+ ///tabulation///
+////////////////
+  public int numTrees(int n) {
+    int[] U_BST = new int[n+1];
+    U_BST[0] = 1;
+    U_BST[1] = 1;
+    for(int i=2;i<=n;i++){
+        for(int j=1;j<=i;j++)
+            U_BST[i] += U_BST[j-1] * U_BST[i-j];
+    }
+    return U_BST[n];
+  }
 }
