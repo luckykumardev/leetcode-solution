e5baf72adcf44f5a6ca7734a9d7ecdcd8d04f027
Your Name <you@example.com>
2020-04-30 23:06:38 -0700

    matrix chain multiplication part 2

--- Matrix chain multiplication.java
+++ Matrix chain multiplication.java
@@ -1,7 +1,33 @@
 class CodeBix
  {
+    //tabulation
+ 	public static int MatrixChainOrder(int p[]) 
+    {   
+        int n = p.length;
+        int dp[][] = new int[n][n];
 
-	static int MatrixChainOrder(int p[], int start, int end, int memo[][]) 
+        for (int L=2; L<n; L++) 
+        { 
+            for (int  start =1; start <n-L+1; start++) 
+            { 
+                int end = start+L-1; 
+                if(end == n) break; 
+                dp[start][end] = Integer.MAX_VALUE; 
+                for (int k=start; k<=end-1; k++) 
+                { 
+                    int cost = dp[start][k] + dp[k+1][end] + p[start-1]*p[k]*p[end]; 
+                    if (cost < dp[start][end]) 
+                        dp[start][end] = cost; 
+                } 
+            } 
+        } 
+  
+        return dp[1][n-1];
+    } 
+    
+
+    //memoization
+	public static int MatrixChainOrder(int p[], int start, int end, int memo[][]) 
     { 
         if (start == end) 
             return 0; 
@@ -19,15 +45,17 @@ class CodeBix
         return min; 
     } 
   
+  
     public static void main(String args[]) 
     { 
         Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
-            int cost[] = new int[n];
+            int p[] = new int[n];
             for(int i = 0; i <n; i++)
-             cost[i] = sc.nextInt();
-            int memo[][] = new int[n][n];
-            int ans = MatrixChainOrder(cost, 1, n-1, memo);
+             p[i] = sc.nextInt();
+            // int memo[][] = new int[n][n];
+            // int ans = MatrixChainOrder(p, 1, n-1, memo);        
+            int ans = MatrixChainOrder(p);
             System.out.println(ans);
     }
 
