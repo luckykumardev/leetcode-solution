9e8a7a29bbd9abb3b924ac64cb214559b3413de0
Your Name <you@example.com>
2020-05-04 01:22:28 -0700

    leetcode 476

---
+++ Number Complement.java
@@ -0,0 +1,15 @@
+class Solution {
+     public int findComplement(int num) 
+    {
+        int i = 0;
+        int j = 0;
+        
+        while (i < num)
+        {
+            i += Math.pow(2, j);
+            j++;
+        }
+        
+        return i-num;
+    }
+}
