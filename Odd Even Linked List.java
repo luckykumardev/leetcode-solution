61f60fca40fd00b8b2ca711d4c5017c3d25d1ce3
Your Name <you@example.com>
2020-05-16 01:40:41 -0700

    leetcode 328

---
+++ Odd Even Linked List.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while(odd.next != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
