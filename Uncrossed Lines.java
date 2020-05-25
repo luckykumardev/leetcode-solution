ca234932b38da2831fbbbcd713d1dd4ac6fefa14
Your Name <you@example.com>
2020-05-25 15:28:47 +0530

    leetcode 1035

---
+++ Uncrossed Lines.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxUncrsossedLines(int[] A, int[] B) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        return rec(A, 0, B, 0);
+    }
+    private int rec(int[] A, int i, int[] B, int j){
+        if(i == A.length || j == B.length) return 0;
+        int count = 0;
+        if(A[i] == B[j]) 
+            count = 1+rec(A, i+1, B, j+1);
+        else
+        	count += Math.max(rec(A, i+1, B, j), rec(A, i, B, j+1));
+        return count;
+    }
+}
