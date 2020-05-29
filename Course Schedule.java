6e9d46a829fa410774065dcca265e3c590f6b6b0
Your Name <you@example.com>
2020-05-29 14:41:54 +0530

    207

---
+++ Course Schedule.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+        int[] indegree = new int[numCourses];
+        Queue<Integer> queue = new LinkedList<Integer>();
+        int count = 0;
+        for (int i = 0; i < numCourses; i++) {
+            map.put(i, new ArrayList<Integer>());
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            map.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            indegree[prerequisites[i][1]]++;
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int i : map.get(current)) {
+                if (--indegree[i] == 0) {
+                    queue.add(i);
+                }
+            }
+            count++;
+        }
+        return count == numCourses;
+    }
+}
