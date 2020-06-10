44270f8614214eb3201e7c6070716e9b78b5201e
luckykumardev <luckyu0020kumar@gmail.com>
2020-06-11 00:40:56 +0530

    word ladder

---
+++ word ladder.java
@@ -0,0 +1,39 @@
+class Solution {
+   public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) return 0;
+        HashMap<String, Boolean> Vmap = new HashMap<String, Boolean>();
+       for(int i = 0; i <wordList.size(); i++)
+            Vmap.put(wordList.get(i), false);
+       
+        Queue<String> q = new LinkedList<String>();
+        int length = 1;
+        q.add(beginWord);
+        Vmap.put(beginWord, true);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                String w = q.poll();
+                if (w.equals(endWord)) 
+                     return length;
+                wordMatch(w, Vmap, q);
+            }
+            length++;
+        }
+        return 0;
+    }
+    
+    public void wordMatch(String w, HashMap<String, Boolean> Vmap, Queue<String> q) {
+        for (int i = 0; i < w.length(); i++) {
+            char[] word = w.toCharArray();
+            for (int j = 0; j < 26; j++) {
+                char c = (char) ('a' + j);
+                word[i] = c;
+                String s = String.valueOf(word);
+                if (Vmap.containsKey(s) && Vmap.get(s) == false) {
+                    q.add(s);
+                    Vmap.put(s, true);
+                }
+            }
+        }
+    }
+}
