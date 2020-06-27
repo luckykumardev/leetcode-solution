9071ebf227a57cf315e69a192ff915def4038a23
luckykumardev <luckyu0020kumar@gmail.com>
2020-06-27 19:46:47 +0530

    Word Search

--- perfect square.java
+++ perfect square.java
@@ -1,15 +1,19 @@
 class Solution {
+ 
     public int numSquares(int n) {
-        return rec(n);
+        int memo[] = new int[n+1];
+        return rec(n, memo);
     }
     
-    int rec(int n) {
+    int rec(int n, int memo[]) {
         if (n < 0) return Integer.MAX_VALUE;
         if (n == 0) return 0;
+        if(memo[n] > 0) return memo[n];
         int min = n;
         for (int i=1;i*i<=n;i++) {
-            min = Math.min(rec(n-(i*i)), min);
+            min = Math.min(rec(n-(i*i), memo), min);
         }
+        memo[n] = min+1;
         return min+1;
     }
 }
