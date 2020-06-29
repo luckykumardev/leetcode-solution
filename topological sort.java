4a7a073d33c616aa9b2a2575e6880f0e7965b910
luckykumardev <luckyu0020kumar@gmail.com>
2020-06-30 01:04:56 +0530

    Friend Circles

--- topological sort.java
+++ topological sort.java
@@ -1,15 +1,13 @@
 class TopologicalSort {
     static int[] topoSort(ArrayList<ArrayList<Integer>> adj, int N) {
-        // add your code here
-        
-        
+
         int ans[] = new int[N];
         Stack<Integer> s = new Stack<>();
         boolean visited[] = new boolean[N];
         for(int i =0; i <N; i++)
           if(visited[i] == false){
            visited[i] = true;
-           topo(i, visited, adj, ans, s);
+           topo(i, visited, adj, s);
           }
          int k =0;
          while(!s.isEmpty())
@@ -17,11 +15,11 @@ class TopologicalSort {
         return ans;
     }
     
-    private static void topo(int i, boolean  visited[], ArrayList<ArrayList<Integer>> adj, int ans[], Stack<Integer> s){
+    private static void topo(int i, boolean  visited[], ArrayList<ArrayList<Integer>> adj, Stack<Integer> s){
         for(int x : adj.get(i)){
             if(visited[x] == false){
                 visited[x] = true;
-                topo(x, visited, adj, ans, s);
+                topo(x, visited, adj, s);
             }
         }
         s.push(i);
