f2cfbf16e02262e26e5e3958b54c4eb005587040
luckykumardev <luckyu0020kumar@gmail.com>
2020-08-26 13:56:42 +0530

    Fizz Buzz

---
+++ Fizz Buzz.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for(int i =1; i <=n; i++){
+           if(i%3 == 0 && i%5 ==0) ans.add("FizzBuzz");
+           else if(i%3 == 0)       ans.add("Fizz");
+           else if(i%5 == 0)       ans.add("Buzz");
+           else                    ans.add(i+"");
+       }
+        
+        return ans;
+    }
+}
